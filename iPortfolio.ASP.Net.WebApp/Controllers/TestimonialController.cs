4286bc6fb56db9204fb84d6c57eafcdc4719a047
huseyincorakli <huseyincorakli@icloud.com>
2022-06-30 02:10:48 +0300

    Testimonial(CRUD)

--- iPortfolio.ASP.Net.WebApp/Controllers/TestimonialController.cs
+++ iPortfolio.ASP.Net.WebApp/Controllers/TestimonialController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,45 @@ namespace iPortfolio.ASP.Net.WebApp.Controllers
     public class TestimonialController : Controller
     {
         TestimonialManager testimoManager = new TestimonialManager(new EfTestimonialDal());
-        // GET: Testimonial
+        
         public ActionResult Testimonial()
         {
             var item = testimoManager.GetList();
             return View(item);
         }
+
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var item = testimoManager.GetById(id);
+            testimoManager.TestimonialDelete(item);
+            return RedirectToAction("Testimonial");
+        }
+
+        [HttpGet]
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTestimonial(Testimonial p)
+        {
+            testimoManager.TestimonialAdd(p);
+            return RedirectToAction("Testimonial");
+        }
+
+        [HttpGet]
+        public ActionResult EditTestimonial(int id)
+        {
+            var item = testimoManager.GetById(id);
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult EditTestimonial(Testimonial p)
+        {
+            testimoManager.TestimonialUpdate(p);
+            return RedirectToAction("Testimonial");
+           
+        }
+
     }
 }
